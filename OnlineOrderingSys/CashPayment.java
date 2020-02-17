55dc8a6788325f00bc0421b3326f799fc5f3c991
Sai Srivathsa Muthyala <muthyalasaisrivathsa@gmail.com>
2020-02-17 23:03:51 +0530

    OOPS Example 3

--- OnlineOrderingSys/CashPayment.java
+++
@@ -1,7 +0,0 @@
-public class CashPayment extends Payment{
-	public void makePayment() {
-		Cart c = new Cart();
-		System.out.println("You can pay" + c.bill() + " on delivery. Thank you.");
-		System.out.println("Item will be delivered soon!!");
-	}
-}
